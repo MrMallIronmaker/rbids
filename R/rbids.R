a5b4d0a23bb522fc6374889369e7eca6958ea128
sfang <snapefung@gmail.com>
2025-02-03 13:44:16 -0600

    Add internal keyword

--- R/rbids.R
+++ R/rbids.R
@@ -123,6 +123,7 @@ print.bids_dataset <- function(bd) {
 #' @title Check if it's a bids object
 #'
 #' @importFrom rlang abort
+#' @keywords Internal
 .bids_obj_checker <- function(bd) {
   if (!inherits(bd, "bids_dataset")) {
     abort("The object provided is not a 'bids_dataset'")
